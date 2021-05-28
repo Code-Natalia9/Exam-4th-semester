c5ce3db435a63c20862864f1c82a2eb4db8979d4
Szalay Regina <64595488+MiraAquaRegina@users.noreply.github.com>
2021-05-28 15:28:29 +0200

    contact-form.php
    comments in code

--- contactform.php
+++ contactform.php
@@ -1,5 +1,5 @@
 <?php
-    $name = $_POST ['name'];
+    $name = $_POST ['name']; // name from html classified, what is what
     $visitor_email = $_POST ['email'];
     $zoom = $_POST ['zoom'];
     $message = $_POST ['message'];
@@ -10,15 +10,15 @@
     $email_body = "User Name: $name.\n".
                     "User Email: $visitor_email.\n".
                         "User Zoom: $zoom.\n".
-                            "User Message: $message.\n";
+                            "User Message: $message.\n"; // Requered 
 
-    $to = "regnat@aquaregina.com";
+    $to = "regnat@aquaregina.com"; // Each email sended form the side, will be resieved on this email (gmail-doesn't work)
 
     $headers = "From: $email_from \r\n";
 
     $headers .= "Reply-To: $visitor_email \r\n";
 
-    mail($to,$email_subject,$email_body,$headers);
+    mail($to,$email_subject,$email_body,$headers); // All informations that sould be resieved
 
-    header ("Location: contact.html");
+    header ("Location: contact.html"); // Which page is this form located at
 ?>
